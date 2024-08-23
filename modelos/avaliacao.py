d72fd370b9401ddc8659e14645ab6f424bf01a0e
Gabi <gabrielamalerba10@gmail.com>
2024-08-23 16:15:13 -0300

    avaliacao.py

--- modelos/avaliacao.py
+++ modelos/avaliacao.py
@@ -1,14 +1,11 @@
 class Avaliacao:
-    def __int__(self, cliente, nota):
-        self._cliente = cliente #nome do cliente que fez a avalição
-        self._nota = nota #nota dada
+    def __init__(self, cliente, nota):
+        self._cliente = cliente  # Nome do cliente que fez a avaliação
+        self._nota = nota  # Nota dada pelo cliente
 
-        #Método para converter o onbjeto Avaliacao em um dicionario para salvar em JSON
-        def __dict__(self):
-            return {
-                'cliente': self._cliente,
-                'nota': self._nota
-            }
-
-
-    
+    # Método para converter o objeto Avaliacao em um dicionário para salvar em JSON
+    def __dict__(self):
+        return {
+            'cliente': self._cliente,
+            'nota': self._nota
+        }
